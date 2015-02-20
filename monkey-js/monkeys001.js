83d7ed10836066d7ab5bd012e9a069369ee13b9c
John Escobedo <letmeshowyou@gmail.com>
2015-02-19 22:58:19 -0600

    added how many guesses in final and sub streaks.

--- monkey-js/monkeys001.js
+++ monkey-js/monkeys001.js
@@ -2,10 +2,12 @@
 // and it's length and an index to the string
 function typewriter(string)
 {
-	this.story = string.split("");
+	this.string = string;
+	this.story = this.string.split("");
 	this.length = this.story.length;
 	this.index = 0;
 	this.best_index = 0;
+	this.guess_count = 0;
 	this.incomplete = true;
 
 // Returns a random index of the story
@@ -44,6 +46,7 @@ function typewriter(string)
 	{
 		// Assign random guess to object's "letter"
 		this.letter = this.story[this.randomIndex()];
+		this.guess_count++;
 
 		// If letter matches current index call matched()
 		// else call mismatched()
@@ -54,10 +57,12 @@ function typewriter(string)
 			this.mismatched();
 		};
 
-		//check to see if new best index reached, reset index
+		// check to see if new best index reached,
+		// print latest streak
+		// reset index
 		if (this.index > this.best_index)
 		{
-			console.log(this.story[this.best_index]);
+			console.log(this.story[this.best_index] + " " + this.guess_count + " guesses.");
 			this.best_index = this.index;
 
 		};
@@ -66,10 +71,10 @@ function typewriter(string)
 
 
 // Create new object, send it "abc"
-var abc = new typewriter("abcdef");
+var abc = new typewriter("abcdefg");
 
 while (abc.incomplete){
 	abc.guess();
 };
 
-console.log("You win");
+console.log(abc.string + " printed after " + abc.guess_count + " guesses.");
