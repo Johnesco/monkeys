2621ad13188b4207fac3a8521bf286ae84c6800d
John E <jescobedo@c-s-d.org>
2015-03-23 04:35:22 -0500

    monkeys000.html

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ I'm writing this in several langauge to both learn those languages and improve m
 
 Updates:
 ----
-
+monkey-js/monkeys003.js:
+Putting the code through JLINT and seeing how much I can clean it up, then will start to refactor parts that need it, and try to DRY up the code more.
 
 monkey-js/monkeys002.js:
 Now that I've tested out OO syntax, time to try to use it better. Correcting errors and changing to prototype syntax. Bug fix: entire alphabet was not being used for guesses making short strings faster to match. Also adding a loop to print out what string subset (discovered) has been found so far. 
---
+++ node/monkeys002.js
@@ -0,0 +1,76 @@
+// Typewriter object
+// holds string and it's length and an counter to the string
+var Typewriter = function (string) {
+  this.string = string;
+  this.story = string.split("");
+  this.length = this.story.length;
+  this.alphabet = ("abcdefghijklmnopqrstuvwxyx").split("");
+  this.counter = 0;
+  this.best_count = 0;
+  this.guess_count = 0;
+  this.incomplete = true;
+};
+
+// Returns a random letter of the alphabet
+Typewriter.prototype.randomcounter = function () {
+  return Math.floor(Math.random() * this.alphabet.length)
+};
+  
+
+// Called when a match is found
+// advances counter
+Typewriter.prototype.correct = function () {
+  // check to see if final letter matched.
+  if (this.counter === this.length-1)
+  {
+    this.incomplete = false;
+  }
+
+  // increase counter
+  this.counter++;
+};
+
+
+Typewriter.prototype.incorrect = function () {
+  this.counter = 0;
+};
+
+
+Typewriter.prototype.guess = function () {
+  this.letter = this.alphabet[this.randomcounter()];
+  this.guess_count++;
+
+  if (this.letter === this.story[this.counter]){
+      this.correct();
+  } else { 
+      this.incorrect();
+  }
+
+  // check to see if new best counter reached,
+  // print latest streak and guesses taken
+  if (this.counter > this.best_count){
+
+    //loop out how much has been found
+    var discovered = "";
+    for (var i=0; i <= this.best_count; i++){
+      discovered+= this.story[i];
+    }
+
+    console.log('"' + discovered + '"' + " string discovered after " + this.guess_count + " guesses.");
+    this.best_count = this.counter;
+  }
+};
+
+Typewriter.prototype.finalAnswer = function () {
+  return ("Monkey1 printed " + this.string + " after " + this.guess_count + " guesses.");
+};
+
+
+// Create new typewriter object, send it a "story"
+var Monkey1 = new Typewriter("hellow");
+
+while (Monkey1.incomplete){
+  Monkey1.guess();
+}
+
+console.log(Monkey1.finalAnswer());
